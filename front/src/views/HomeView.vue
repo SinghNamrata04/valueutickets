f76bb13f238a4676a1fd82e874045125ea1ce669
rmonstr <anish.29an@gmail.com>
2025-01-28 11:42:58 +0530

    developed the flightsearch and offers components

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
 import TheWelcome from '../components/TeamGreeting.vue'
+import FlightSearch from '../components/FlightSearch.vue'
+import offers from '../components/offers.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <FlightSearch />  <!-- You can add comments like this within the template -->
+    <offers/>
   </main>
 </template>
