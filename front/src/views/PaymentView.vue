3d2f3fccaea2fcff18ad0e022ba11407c1b5d20b
Shivam Tripathi <shivamtripathi93758@gmail.com>
2025-02-03 21:58:40 +0530

    complete Task
    1-Premium Support
    2-Booking Number

--- front/src/views/PaymentView.vue
+++ front/src/views/PaymentView.vue
@@ -1,11 +1,15 @@
 <script setup>
 import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
+import PremiumSupport from '../components/PremiumSupp.vue'
+import BookingNumSMS from '../components/BookingNumSMS.vue'
 </script>
 
 <template>
   <main>
     <Header />
+    <PremiumSupport/>
+    <BookingNumSMS/>
     <Footer/>
   </main>
 </template>
